f5f1719bdf34d62235b0991f6eae7bbb4401ce0b
Hanajannath <hanahana16027@gmail.com>
2025-10-06 17:35:41 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+a=int(input("enter first number: "))
+b=int(input("enter second number: "))
+sum=a+b
+difference=a-b
+product=a*b
+floatdivsion=a/b
+integerdivision=a//b
+remainder=a%b
+print("sum is:",sum)
+print("difference is:",difference)
+print("product is:",product)
+print("quotient is:",floatdivsion)
+print("quotient without decimal is:",integerdivision)
+print("remainder is:",remainder)
+a1=str(a)
+b1=str(b)
+concatenate=a1+b1
+print("concatenated value is:",concatenate)
